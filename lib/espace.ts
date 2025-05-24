ef8afb5a8e8bde5659f9f07134888ad5cf16745e
freudpunisher <punishergte@gmail.com>
2025-05-24 18:44:41 +0200

    change endpojnts in espaces.ts

--- lib/espace.ts
+++ lib/espace.ts
@@ -1,12 +1,12 @@
 // lib/espaces.ts
 export async function getAllEspaceSlugs(): Promise<string[]> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/espaces`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/shops`)
     const data: { slug: string }[] = await res.json()
     return data.map((e) => e.slug)
   }
   
   export async function getEspaceBySlug(slug: string) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/espaces/${slug}`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/shops/${slug}`)
     if (!res.ok) return null
     return res.json()
   }
